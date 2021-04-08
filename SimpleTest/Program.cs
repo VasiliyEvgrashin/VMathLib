fb57f149e72d4de99d5123ca227d0147534159b2
vevgrashin <vevgrashin@primetsr.com>
2021-04-08 12:19:27 -0500

    add SimpleTest

---
+++ SimpleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMath;
+
+namespace SimpleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<double> x = new List<double>() { 0, 1, 6, 12 };
+            IList<double> y = new List<double>() { 8, 10, 7, 0 };
+            ABSolver aBSolver = new ABSolver(x, y, 23);
+            FXFY res = aBSolver.Solve(1);
+            using (StreamWriter file = new StreamWriter(@"C:\dell\test.txt"))
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    file.WriteLine(res.FX[i] + ";" + res.FY[i]);
+                }
+            }
+            FurSolver furSolver = aBSolver.GetSolver();
+            double yj = furSolver.CalcY(2234243);
+            Console.ReadKey();  
+        }
+    }
+}
